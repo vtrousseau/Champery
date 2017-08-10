76d65bf860d952ffcc13ad79af6399ce268b2ff3
vtrousseau <vtrousseau@gmail.com>
2017-08-10 23:20:31 +0100

    Add files via upload

---
+++ js/activites.js
@@ -0,0 +1,10 @@
+$(".ski").mouseenter(function () {
+document.getElementById("ski").addClass('scale-up');
+	
+	
+	/*
+    $("div.header-menu + div").removeClass("post-filter");
+}).mouseleave(function () {
+    $("div.header-menu + div").addClass("post-filter");
+	*/
+});
