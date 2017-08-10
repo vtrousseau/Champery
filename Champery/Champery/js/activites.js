216b11d9c0cd7d0623eee7dd0391593df09d616a
vtrousseau <vtrousseau@gmail.com>
2017-08-10 23:26:36 +0100

    Add files via upload

---
+++ Champery/Champery/js/activites.js
@@ -0,0 +1,41 @@
+$(".labyrinthe").hover(function () {
+$("#labyrinthe").toggleClass('white');
+});
+
+$(".foundation").hover(function () {
+$("#foundation").toggleClass('white');
+});
+
+$(".abbaye").hover(function () {
+$("#abbaye").toggleClass('white');
+});
+
+$(".grotte").hover(function () {
+$("#grotte").toggleClass('white');
+});
+
+$(".baths").hover(function () {
+$("#baths").toggleClass('white');
+});
+
+$(".saline").hover(function () {
+$("#saline").toggleClass('white');
+});
+
+$(".aigle").hover(function () {
+$("#aigle").toggleClass('white');
+});
+
+$(".swissvapeurparc").hover(function () {
+$("#swissvapeurparc").toggleClass('white');
+});
+
+$(".aquaparc").hover(function () {
+$("#aquaparc").toggleClass('white');
+});
+
+$(".chillon").hover(function () {
+$("#chillon").toggleClass('white');
+});
+
+
