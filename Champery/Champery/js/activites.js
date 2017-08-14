caec12119f326797ef905cf13a795dd9c76d1dd5
vtrousseau <vtrousseau@gmail.com>
2017-08-14 22:13:24 +0100

    Add files via upload

--- Champery/Champery/js/activites.js
+++ Champery/Champery/js/activites.js
@@ -38,60 +38,24 @@ $(".chillon").hover(function () {
 $("#chillon").toggleClass('white');
 });
 
+var scrollEventHandler = function() {
+	if(isScrolledIntoView(document.getElementsById('footer')[0])) {
+  	$('.map').fadeOut();
+  } else {
+  	$('.map').fadeIn();
+  }  
+}
 
-(function($) {
+function unbindScrollEventHandler() {
+	$(document).unbind('scroll', scrollEventHandler);
+}
 
-  /**
-   * Copyright 2012, Digital Fusion
-   * Licensed under the MIT license.
-   * http://teamdf.com/jquery-plugins/license/
-   *
-   * @author Sam Sehnert
-   * @desc A small plugin that checks whether elements are within
-   *     the user visible viewport of a web browser.
-   *     only accounts for vertical position, not horizontal.
-   */
+$(document).scroll(scrollEventHandler);
 
-  $.fn.visible = function(partial) {
-    
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height(),
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
-    
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+function isScrolledIntoView(el) {
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
 
-  };
-    
-})(jQuery);
-
-var win = $(window);
-
-var footer = $("#trigger");
-
-
-footer.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-  $(".map").fadeOut();} 
-	else {
-	$(".map").fadeIn();
-	} 
-});
-/*
-win.scroll(function(event) {
-  
- footer.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-  $(".map").fadeOut();} 
-	else {
-	$(".map").fadeIn();
-	} 
-    }) 
-  });*/
- 
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
