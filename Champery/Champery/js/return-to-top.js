d52f6b5dea77b356bb2e4585843f96dd4700aafd
vtrousseau <vtrousseau@gmail.com>
2017-08-20 23:05:06 +0100

    Add files via upload

--- Champery/Champery/js/return-to-top.js
+++ Champery/Champery/js/return-to-top.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function() {
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
@@ -11,4 +11,5 @@ $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
+});
 });
